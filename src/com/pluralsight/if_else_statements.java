7d6eb9c1d8d43a1e76944e1eeb4a90124e057180
MokuKen <imsilencer44@gmail.com>
2026-04-04 23:23:00 +0800

    added if else statements

---
+++ src/com/pluralsight/if_else_statements.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class if_else_statements {
+    public static void main(String[] args) {
+        int value1 = 20;
+        int value2 = 15;
+
+        if (value1 < value2) {
+            System.out.println("Value 1 is less than value2");
+        } else if(value1 > value2) {
+            System.out.println("Value 1 is greate than value2");
+        } else {
+            System.out.println("hi");
+        }
+    }
+}
