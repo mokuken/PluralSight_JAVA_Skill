d83045ae0c9ca64191dace6ca2abe09540e69879
MokuKen <imsilencer44@gmail.com>
2026-04-04 23:22:49 +0800

    added logical operators

---
+++ src/com/pluralsight/logical_operators.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class logical_operators {
+    public static void main(String[] args) {
+        String person1 = "white";
+        String person2 = "white";
+
+        if (person1 == "white" & person2 == "white")
+            System.out.println("They can be together");
+        else
+            System.out.println("They cannot be together");
+
+
+    }
+}
