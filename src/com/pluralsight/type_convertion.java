bb7af5ba9c9aa2ad4cdc767c7e854f429c77d19c
MokuKen <imsilencer44@gmail.com>
2026-04-04 23:21:58 +0800

    changes to type_convertion

--- src/com/pluralsight/type_convertion.java
+++ src/com/pluralsight/type_convertion.java
@@ -10,6 +10,18 @@ public class type_convertion {
         long longValueTwo = 50;
         int intValueTwo = (int) longValueTwo;
 
+        // to make the two explicit
+        short intValueThree = (short) (longValueOne + longValueTwo);
 
+        // closer look to type convertion
+        float floatVal = 1.0f;
+        double doubleVal = 4.0d;
+        byte byteVal = 7;
+        short shortVal = 7;
+        long longVal = 5;
+
+        short result1 = (short) longVal;
+        short result2 = (short) (byteVal + longVal);
+        float result3 = longVal - floatVal; // better to use float if you have a value of float
     }
 }
