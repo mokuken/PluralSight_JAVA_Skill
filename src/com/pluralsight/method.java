f7342dc909aa0346d5a93662487595ff5e09c989
MokuKen <imsilencer44@gmail.com>
2026-04-05 17:24:03 +0800

    added method

---
+++ src/com/pluralsight/method.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class method {
+    static void doSomething() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        doSomething();
+    }
+}
