c42d324097b102cc77bd549897fbc9d9a7200f98
MokuKen <imsilencer44@gmail.com>
2026-04-05 17:24:35 +0800

    added do while loop

---
+++ src/com/pluralsight/do_while_loop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class do_while_loop {
+    public static void main(String[] args) {
+        int someValue = 3;
+
+
+        do {
+            System.out.println(someValue);
+            someValue--;
+        } while (someValue != 0);
+    }
+}
